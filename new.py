0c1494553332887ebb7c06ee683935bbb0085ca6
mirageoasis <kimhw0820@naver.com>
2022-09-01 18:18:23 +0900

    new.py

---
+++ new.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import multi_infer
+
+
+PATH = "./test.txt"
+
+RESULT_PATH="graph.txt"
+DIFF_PATH="diff.txt"
+TEMP_PATH='temp.txt'
+TEMP_WRITE='temp_write.txt'
+
+f = open(TEMP_PATH, 'r')
+f1 = open(TEMP_WRITE, 'w')
+
+lines = f.readlines()
+
+for line in lines:
+    if multi_infer.judge(line) == 1:
+        print(line.strip(), file=f1)
+
+
+
+f.close()
+f1.close()
+
+#multi_infer.infer()

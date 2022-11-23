50e0a27d4c1c356f8b463bc42a7f015bc3bc9746
FishSoup <whdqhr345@gmail.com>
2022-11-24 06:56:27 +0900

    개조

--- infer/multi_infer.py
+++ infer/multi_infer.py
@@ -8,6 +8,10 @@ import json
 from glob import glob
 from clean import clean
 
+from kafka import KafkaConsumer, KafkaProducer
+from time import sleep
+
+import json
 
 # inferTest 불러오기
 root_path = sys.path[0]
@@ -20,10 +24,39 @@ from multi_task_model import Model
 
 #map location 해주면 환경 바뀌어도 가능
 def main():
+    topic_name = "new_comment"
+    consumer = KafkaConsumer(
+        topic_name,
+        bootstrap_servers=[
+            "localhost:9092",
+        ],
+        auto_offset_reset="latest",
+        enable_auto_commit=True,
+        group_id="infer-group",
+        # value_deserializer=lambda x: loads(x.decode("utf-8")),
+        consumer_timeout_ms=1000,
+    )
+    producer = KafkaProducer(
+        acks=0,
+        bootstrap_servers=[
+            'localhost:9092'
+        ],
+    )
+    print("start")
     while True:
-        sentence = input("문장을 입력하시오! ")
-        judge(sentence=sentence)
-    
+        for message in consumer:
+            print('===========')
+
+            data = json.loads(message.value.decode('utf-8'))
+
+            print(data['data']['comment']['memo'])
+            data['infer'] = judge(data['data']['comment']['memo'])
+            
+            producer.send('add-comment', bytes(json.dumps(data), 'utf-8'))
+            producer.flush()
+
+        sleep(0.1)
+
 def infer(x):
     return model(**model.tokenizer(x, return_tensors='pt'))
 def judge(sentence):
@@ -36,6 +69,7 @@ def judge(sentence):
         #print(test_prediction)
         output = torch.sigmoid(test_prediction[1])
         output = output.detach().flatten().numpy()
+        output = list(map(float, output))
         #for i in zip(LABEL_COLUMNS, output):
             #print(f"{i[0]} {i[1]}에 해당합니다")
                 #return 1
